c92eeb134372a9d0b7c91ac78018f86fc979c5f1
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-12 22:51:00 -0400

    Code along completed, v.01.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
+# This is a code-along exercise with code coming
+#  from the problem statement.
+
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
+# This is a code-along exercise with code coming
+#  from the problem statement.
+
 class Meal
 
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
 end
